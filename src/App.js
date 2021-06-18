05de0ec0839b697d72996bbf49aea38cdda6bdc1
DESKTOP-8F8DSJD\DELL <priyadarshini.tarai123@gmail.com>
2021-06-19 01:39:22 +0530

    redux-my-duty

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React, { useEffect } from 'react';
 import { Provider } from 'react-redux'
 import MainNavigation from './navigation/mainNavigation';
 import { View } from 'react-native';
-import configureStore from './configureStore'
+import store  from './configureStore'
 
-const store = configureStore()
+// const store = configureStore()
 const App = () => {
   const [showSplash, setShowSplash] = React.useState(true);
   useEffect(() => {
@@ -12,7 +12,14 @@ const App = () => {
       setShowSplash(false);
     }, 2000);
   }, []);
-  return showSplash ? <View /> :  <MainNavigation/>;
+  // return showSplash ? <View /> :  <MainNavigation/>;
+  // return ( <View>showSplash ? <View /> : <View><Provider store={store}> <MainNavigation/></Provider></View><View />)
+  return (
+    
+        <Provider store = {store} >
+        <MainNavigation />
+        </Provider>
+)
 };
 
 export default App;
--- src/configureStore.js
+++ src/configureStore.js
@@ -1,13 +1,21 @@
-import { applyMiddleware, createStore } from 'redux'
+import { applyMiddleware } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import rootReducer from './reducer'
+import { createStore } from 'redux'
+// import notesReducer from './notesApp'
 
-export default function configureStore(persistedState) {
-  const store = createStore(
-    rootReducer,
-    persistedState,
-    applyMiddleware(thunkMiddleware)
-  )
-  return store
-}
+const store = createStore(rootReducer)
+
+
+// export default function configureStore(persistedState) {
+//   const store = createStore(
+//     rootReducer,
+//     persistedState,
+//     applyMiddleware(thunkMiddleware)
+//   )
+//   return store
+// }
+
+export default store
+
 
